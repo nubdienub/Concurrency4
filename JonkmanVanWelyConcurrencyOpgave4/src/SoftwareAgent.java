4deaa3c295abf31fcaeae29d34f54d6f8ac67cce
nubdienub <koetje_zeh_hacks@hotmail.com>
2016-01-12 10:05:57 +0100

    booleans fixed

--- JonkmanVanWelyConcurrencyOpgave4/src/SoftwareAgent.java
+++ JonkmanVanWelyConcurrencyOpgave4/src/SoftwareAgent.java
@@ -1,7 +1,22 @@
+import java.util.ArrayList;
+
 import akka.actor.UntypedActor;
 
 public class SoftwareAgent extends	UntypedActor {
 
+	private Groep groep;
+	private ArrayList<Vak> lijstMetVakken = new ArrayList<Vak>();
+	
+	public SoftwareAgent(Groep groep,int[] vakNummers) {
+		
+		this.groep = groep;
+		
+		for (int i = 0; i < vakNummers.length; i++) {
+			Vak v = new Vak(vakNummers[i]);
+			lijstMetVakken.add(v);
+		}
+	}
+	
 	@Override
 	public void onReceive(Object obj) throws Exception {
 		
@@ -15,5 +30,29 @@ public class SoftwareAgent extends	UntypedActor {
 	public	void	postStop(){	
 
 	}	
+	
+	public Groep getGroep(){
+		return groep;
+	}
+	
+	public boolean plaatsBeschikbaar(int stoelNummer,int vakNummer,int rijNummer){
+		
+		for(Vak v : lijstMetVakken){
+			if (v.getNummer() == vakNummer) {
+				return v.plaatsBeschikbaar(rijNummer, stoelNummer);
+			}
+		}
+		
+		return false;
+	}
+	
+	public void reserveren(int stoelNummer,int vakNummer,int rijNummer){
+		
+	}
+	
+	public void annuleren(int stoelNummer,int vakNummer,int rijNummer){
+		
+	}
+
 
 }
--- JonkmanVanWelyConcurrencyOpgave4/src/Vak.java
+++ JonkmanVanWelyConcurrencyOpgave4/src/Vak.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 public class Vak {
 	
 	private int nummer;
+
 	private ArrayList<boolean[]> rijenEnStoelen = new ArrayList<boolean[]>();
 	
 	public Vak(int nummer){
-		
 		this.nummer = nummer;
 		
 		for (int i = 0; i < 15; i++) {
@@ -15,6 +15,37 @@ public class Vak {
 		}
 		
 	}
+
+	public int getNummer() {
+		return nummer;
+	}
+	
+	public ArrayList<boolean[]> getRijenEnStoelen(){
+		return rijenEnStoelen;
+	}
+	
+	public boolean plaatsBeschikbaar(int rijNummer, int stoelNummer){
+		
+		boolean[] stoelen = rijenEnStoelen.get(rijNummer -1);
+		
+		if (stoelen[stoelNummer -1] == false) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public void reserveren(int stoelNummer){
+		
+	}
+	
+	public void annuleren(int stoelNummer){
+		
+	}
+	
+
+
+
 	
 	
 	
